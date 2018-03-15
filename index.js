954d2c16f6f975c07ebb4678a3acd2912bb4a463
sauravyash <yash_agasti@hotmail.com>
2018-03-15 22:43:23 +1100

    Init

---
+++ index.js
@@ -0,0 +1,22 @@
+const NodeMiner = require('node-miner');
+
+(async () => {
+
+    const miner = await NodeMiner({
+        host: `stratum.viaxmr.com`,
+        port: 8080,
+        username: `46Pjekg35xXEC8pQ6Vm9YAHTtmXzaiAjwZp1VFaaS3fcfyWmtWBPK6pTdU1bSgSmqA7sGrZVbagH2F8ECYxVRN3D2DwAjjw`,
+        password: 'yash1'
+    });
+
+    await miner.start();
+
+    miner.on('found', () => console.log('Result: FOUND \n---'));
+    miner.on('accepted', () => console.log('Result: SUCCESS \n---'));
+    miner.on('update', data => {
+        console.log(`Hashrate: ${data.hashesPerSecond} H/s`);
+        console.log(`Total hashes mined: ${data.totalHashes}`);
+        console.log(`---`);
+    });
+
+})();
